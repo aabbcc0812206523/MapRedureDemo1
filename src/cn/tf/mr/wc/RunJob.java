8b2c76fa7a7695d0ff3eee3bfe27453e52c3b82b
sdksdk0 <690644511@qq.com>
2016-12-19 10:51:28 +0800

    hdfs

---
+++ src/cn/tf/mr/wc/RunJob.java
@@ -0,0 +1,57 @@
+package cn.tf.mr.wc;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class RunJob {
+	
+	public static void main(String[] args) {
+		Configuration config=new Configuration();
+		config.set("fs.defaultFS", "hdfs://192.168.44.140:8020");
+		//config.set("yarn.resourcemanager.hostname", "node1");
+		config.set("mapred.jar", "C:\\Users\\asus\\Desktop\\wc.jar");
+		
+		try {
+			FileSystem fs =FileSystem.get(config);
+			
+			Job job=Job.getInstance(config);
+			
+			job.setJarByClass(RunJob.class);
+			job.setJobName("wc");
+			job.setMapperClass(WordCountMapper.class);
+			job.setReducerClass(WordCountReduce.class);
+			
+			job.setMapOutputKeyClass(Text.class);
+			job.setMapOutputValueClass(IntWritable.class);
+			
+			FileInputFormat.addInputPath(job, new Path("/usr/input/"));
+			
+			Path outpath =new Path("/usr/output/wc");
+			if(fs.exists(outpath)){
+				fs.delete(outpath, true);
+			}
+			FileOutputFormat.setOutputPath(job, outpath);
+			
+			boolean f= job.waitForCompletion(true);
+			if(f){
+				System.out.println("job任务执行成功");
+			}
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
